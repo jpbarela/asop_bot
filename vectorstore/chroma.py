9d4004fa6c521d51f55ec09aac601db740d26ef5
JP Barela <barela@jpbarela.com>
2023-07-08 11:03:00 -0600

    Add persistent datastore

--- vectorstore/chroma.py
+++ vectorstore/chroma.py
@@ -4,10 +4,19 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.schema.document import Document
 
+from pathlib import Path
+import os
+
 embedding_function = SentenceTransformerEmbeddings(
     model_name="all-MiniLM-L6-v2")
 
-db = Chroma(embedding_function=embedding_function)
+persist_folder = str(Path(__file__).parent.joinpath("chroma_data"))
+
+if os.environ.get("TESTING"):
+    persist_folder = None
+
+db = Chroma(embedding_function=embedding_function,
+            persist_directory=persist_folder)
 
 
 def save_documents(docs: List[Document]) -> None:
